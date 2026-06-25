478283341552233be1afbb091706da128b057c3e
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-06-25 11:27:12 +0200

    refactor routetester

--- Assets/Scripts/RouteTester.cs
+++ Assets/Scripts/RouteTester.cs
@@ -7,8 +7,6 @@ public class RouteTester : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private CityGenerator cityGenerator;
     private RouteGenerator routeGenerator;
-    Address startAddress;
-    Address endAddress;
     void Start()
     {
         cityGenerator = GetComponent<CityGenerator>();
@@ -35,17 +33,23 @@ public class RouteTester : MonoBehaviour
         Debug.Log("Testing route from " + startAddress.streetName + " " + startAddress.number +
                   " to " + endAddress.streetName + " " + endAddress.number);
         List<Road> roadRoute = routeGenerator.GetRoute(startAddress, endAddress);
-        // if (roadRoute != null)
-        // {
-        //     Debug.Log("Route found with " + roadRoute.Count + " roads.");
-        //     foreach (Road road in roadRoute)
-        //     {
-        //         Debug.Log(road.streetName + " at grid (" + road.gridX + ", " + road.gridZ + ")");
-        //     }
-        // }
-        // else
-        // {
-        //     Debug.Log("No route found.");
-        // }
+
+        // PrintRoute(roadRoute);
+    }
+
+    void PrintRoute(List<Road> roadRoute)
+    {
+        if (roadRoute != null)
+        {
+            Debug.Log("Route found with " + roadRoute.Count + " roads.");
+            foreach (Road road in roadRoute)
+            {
+                Debug.Log(road.streetName + " at grid (" + road.gridX + ", " + road.gridZ + ")");
+            }
+        }
+        else
+        {
+            Debug.Log("No route found.");
+        }
     }
 }
